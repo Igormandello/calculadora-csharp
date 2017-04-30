f277f63967da164be7cf9402bb36f4eea6a75bd9
Igormandello <igormandello@gmail.com>
2017-04-30 05:02:05 -0300

    Adicionado projeto e feito visual
    Criado projeto em C# e criado visual da calculadora.

---
+++ Calculadora/Calculadora/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 35);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 53);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(324, 405);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 470);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public static readonly string[][] txtBotoes = {
+                                                        new string[] { "(", ")", "^", "C" },
+                                                        new string[] { "7", "8", "9", "/" },
+                                                        new string[] { "4", "5", "6", "*" },
+                                                        new string[] { "1", "2", "3", "-" },
+                                                        new string[] { "0", ".", "=", "+" }
+                                                      };
+
+        private Button[][] botoes;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            botoes = new Button[txtBotoes.Length][];
+            for (int n = 0; n < botoes.Length; n++)
+            {
+                botoes[n] = new Button[txtBotoes[0].Length];
+                for (int i = 0; i < botoes[n].Length; i++)
+                {
+                    botoes[n][i] = new Button();
+                    botoes[n][i].Text = txtBotoes[n][i];
+                    botoes[n][i].Size = new Size(75, 75);
+                    botoes[n][i].Margin = new Padding(3, 3, 3, 3);
+                    botoes[n][i].Click += btnClick;
+                    this.flowLayoutPanel1.Controls.Add(botoes[n][i]);
+                }
+            }
+        }
+
+        private void btnClick(object sender, EventArgs e)
+        {
+            textBox1.Text += ((Button)sender).Text;
+        }
+    }
+}
